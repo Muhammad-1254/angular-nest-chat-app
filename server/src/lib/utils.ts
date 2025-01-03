6e2546660f90996e4e460fba127fbf3be6d5de0d
Usman Soomro <usmansoomro1234@gmail.com>
2025-01-04 00:03:54 +0500

    changing cookie options

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -4,10 +4,10 @@ import { CookieOptions } from "express";
 
 export const cookieOptions:CookieOptions = {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === 'production',
-    secure: false,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
+    domain:process.env.FRONTEND_URL
 }
 
 
