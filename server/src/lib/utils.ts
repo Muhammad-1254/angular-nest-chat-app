728523bccaf0ba56a00e2a65b9f06585c96e67fd
Usman Soomro <usmansoomro1234@gmail.com>
2025-01-04 00:27:04 +0500

    changing cookkie options

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,13 +1,14 @@
 import { CookieOptions } from "express";
 
-
+const isProd =  process.env.NODE_ENV === 'production'
 
 export const cookieOptions:CookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    secure: isProd,
+    sameSite: isProd?'none':'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days,
-    domain:process.env.FRONTEND_URL
+    domain:process.env.FRONTEND_URL,
+    path:'/'
 }
 
 
